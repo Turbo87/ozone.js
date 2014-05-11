f5c3a4fcd28480b59b131a0ec8daeccba4b7406c
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-05-11 13:37:21 +0200

    Added empty O3 class definition

---
+++ src/ozone.js
@@ -0,0 +1,23 @@
+var O3 = function(id, opt_options) {
+};
+
+O3.prototype.setOptions = function(options) {
+}
+
+O3.prototype.setZone = function(zone) {
+};
+
+O3.prototype.setAngle = function(angle) {
+};
+
+O3.prototype.setLegs = function(prev, next) {
+};
+
+O3.prototype.getEffectiveAngle = function() {
+};
+
+O3.prototype.draw = function() {
+};
+
+if (typeof exports === 'object')
+    module.exports = O3;
